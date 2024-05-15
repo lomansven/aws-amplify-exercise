8761655fe834327b0a2d4d24c02e21f7bb64ab25
Sven Loman <svenloman@home.nl>
2024-05-15 21:15:17 +0200

    Update to trigger build

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello from the updated version!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
